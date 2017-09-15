d1b484507f55b2164438bfb93134323eec7cd42c
tastu teche <tastuteche@yahoo.com>
2017-09-15 14:11:05 +0800

    setup.py added GENTOO_PORTAGE_EPREFIX

--- setup.py
+++ setup.py
@@ -13,15 +13,15 @@
 
 from distutils.core import setup
 
-setup(name          = 'gs-nodepkg',
-      version       = '0.2.1',
-      description   = 'g-sorcery backend for nodepkg packages',
-      author        = 'Tastu Teche',
-      author_email  = 'tastuteche@yahoo.com',
-      packages      = ['gs_nodepkg'],
-      package_data  = {'gs_nodepkg': ['data/*']},
-      scripts       = ['bin/gs-nodepkg-generate-db', 'bin/gs-nodepkg'],
-      data_files    = [('/etc/g-sorcery/', ['gs-nodepkg.json']),
-                       ('/etc/layman/overlays/', ['gs-nodepkg-overlays.xml'])],
-      license       = 'GPL-2',
+setup(name='gs-nodepkg',
+      version='0.2.1',
+      description='g-sorcery backend for nodepkg packages',
+      author='Tastu Teche',
+      author_email='tastuteche@yahoo.com',
+      packages=['gs_nodepkg'],
+      package_data={'gs_nodepkg': ['data/*']},
+      scripts=['bin/gs-nodepkg-generate-db', 'bin/gs-nodepkg'],
+      data_files=[('@GENTOO_PORTAGE_EPREFIX@/etc/g-sorcery/', ['gs-nodepkg.json']),
+                  ('@GENTOO_PORTAGE_EPREFIX@/etc/layman/overlays/', ['gs-nodepkg-overlays.xml'])],
+      license='GPL-2',
       )
